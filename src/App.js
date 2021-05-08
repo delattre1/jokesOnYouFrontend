d7a7c88383db92433554f9413653730ad6c9c5dc
delattre1 <daniieeldelattre@gmail.com>
2021-05-08 15:59:17 -0300

    loading spinner

--- src/App.js
+++ src/App.js
@@ -4,10 +4,14 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import React from 'react';
 import FavoriteJokes from "./components/FavoriteJokes"
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import Loader from "react-loader-spinner";
 
 
 function App() {
   function saveJoke() {
+    setLoading(true);
+
     axios
       .post('https://evening-harbor-15666.herokuapp.com/api/favorites/', joke)
       .then((res) => {
@@ -15,21 +19,29 @@ function App() {
       })
 
     axios
-      .get('https://evening-harbor-15666.herokuapp.com/api/joke/')
-      .then((res) => setJoke(res.data));
+      .get('http://localhost:8000/api/joke/')
+      .then((res) => {
+        setJoke(res.data);
+        setLoading(false);
+      });
   };
 
   const [joke, setJoke] = useState([]);
   const [favs,  setFavs]  = useState([]);
+  const [isLoading,  setLoading]  = useState('');
+
+  useEffect(() => {
+    setLoading(true);
+  }, []);
 
   useEffect(() => {
     axios
       .get('https://evening-harbor-15666.herokuapp.com/api/favorites/')
       .then((res) => {
-        setFavs(res.data)
-      })
-    }, []);
-
+        setLoading(false);
+        setJoke(res.data)
+      });
+  }, []);
 
   useEffect(() => {
     axios
@@ -46,8 +58,15 @@ function App() {
         </div>
       
       <div className="mainContainer">
-        <Joke title={joke.setup}>{joke.delivery}</Joke>
-        <button onClick={saveJoke} >Save Joke</button>
+      {isLoading ? 
+        <Loader type="BallTriangle" color="#00BFFF" height={80} width={80} /> : 
+        (
+        <div>
+          <Joke title={joke.setup}>{joke.delivery}</Joke>
+          <button onClick={saveJoke} >Save Joke</button>
+        </div>
+        )
+       }
 
         <FavoriteJokes favJokes={favs}></FavoriteJokes>
         <img className="jokeImg" src="/mini-joker.jpg" alt="" />
