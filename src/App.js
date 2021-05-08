4cc95eb9328fd2ec1c782085e54c7bddd675e34f
delattre1 <daniieeldelattre@gmail.com>
2021-05-08 15:52:47 -0300

    spinner on fetch

--- src/App.js
+++ src/App.js
@@ -4,37 +4,51 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import React from 'react';
 import FavoriteJokes from "./components/FavoriteJokes"
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import Loader from "react-loader-spinner";
+
 
 
 function App() {
   function saveJoke() {
+    setLoading(true);
     axios
-      .post('https://localhost:8000/api/favorites/', joke)
+      .post('http://localhost:8000/api/favorites/', joke)
       .then((res) => {
         setFavs(res.data)
       })
 
     axios
-      .get('https://localhost:8000/api/joke/')
-      .then((res) => setJoke(res.data));
+      .get('http://localhost:8000/api/joke/')
+      .then((res) => {
+        setJoke(res.data);
+        setLoading(false);
+      });
   };
 
   const [joke, setJoke] = useState([]);
   const [favs,  setFavs]  = useState([]);
+  const [isLoading,  setLoading]  = useState('');
 
   useEffect(() => {
     axios
-      .get('https://localhost:8000/api/favorites/')
+      .get('http://localhost:8000/api/favorites/')
       .then((res) => {
         setFavs(res.data)
       })
     }, []);
-
+  
+  useEffect(() => {
+    setLoading(true);
+  }, []);
 
   useEffect(() => {
     axios
-      .get('https://localhost:8000/api/joke/')
-      .then((res) => setJoke(res.data));
+      .get('http://localhost:8000/api/joke/')
+      .then((res) => {
+        setLoading(false);
+        setJoke(res.data)
+      });
   }, []);
 
   console.log(joke);
@@ -46,9 +60,16 @@ function App() {
         </div>
       
       <div className="mainContainer">
-        <Joke title={joke.setup}>{joke.delivery}</Joke>
-        <button onClick={saveJoke} >Save Joke</button>
 
+      {isLoading ? 
+        <Loader type="BallTriangle" color="#00BFFF" height={80} width={80} /> : 
+        (
+        <div>
+          <Joke title={joke.setup}>{joke.delivery}</Joke>
+          <button onClick={saveJoke} >Save Joke</button>
+        </div>
+        )
+       }
         <FavoriteJokes favJokes={favs}></FavoriteJokes>
         <img className="jokeImg" src="/mini-joker.jpg" alt="" />
       </div>   
